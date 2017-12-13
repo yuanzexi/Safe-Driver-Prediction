1635801db03b8c21c4cf7329bfd60014a29e28fb
zexi yuan <yuanzexi63@gmail.com>
2017-12-13 16:58:26 +0800

    Delete pro_rf copy.py

--- codes/pro_rf copy.py
+++
@@ -1,8 +0,0 @@
-rf = ensemble.RandomForestClassifier(n_estimators=N_forest , max_depth=5, 
-                                 min_samples_leaf=4, n_jobs = -1,  
-                                 max_features='auto', random_state=0, class_weight='balanced_subsample')
-rf.fit(train, target)
-pre_proba = rf.predict_proba(test)
-
-submit = pd.DataFrame({'id':test['id'],'target':pre_proba})
-submit.to_csv('../input/submit.csv',index=False) 
